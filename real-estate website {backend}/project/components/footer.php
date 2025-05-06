13e57f36ed7e19038137bfa4fac3618061c2cc03
Kashi78922 <kashireddy@klebcadwd.com>
2025-05-06 08:13:50 +0530

    Add files via upload

---
+++ real-estate website {backend}/project/components/footer.php
@@ -0,0 +1,36 @@
+<!-- footer section starts  -->
+
+<footer class="footer">
+
+   <section class="flex">
+
+      <div class="box">
+         <a href="tel:1234567890"><i class="fas fa-phone"></i><span>123456789</span></a>
+         <a href="tel:1112223333"><i class="fas fa-phone"></i><span>1112223333</span></a>
+         <a href="mailto:shaikhanas@gmail.com"><i class="fas fa-envelope"></i><span>ABCabc123@gmail.com</span></a>
+         <a href="#"><i class="fas fa-map-marker-alt"></i><span>mumbai, india - 400104</span></a>
+      </div>
+
+      <div class="box">
+         <a href="home.php"><span>Home</span></a>
+         <a href="about.php"><span>About</span></a>
+         <a href="contact.php"><span>Contact</span></a>
+         <a href="listings.php"><span>All listings</span></a>
+         <a href="saved.php"><span>Saved properties</span></a>
+      </div>
+
+      <div class="box">
+         <a href="#"><span>Facebook</span><i class="fab fa-facebook-f"></i></a>
+         <a href="#"><span>Twitter</span><i class="fab fa-twitter"></i></a>
+         <a href="#"><span>Linkedin</span><i class="fab fa-linkedin"></i></a>
+         <a href="#"><span>Instagram</span><i class="fab fa-instagram"></i></a>
+
+      </div>
+
+   </section>
+
+   <div class="credit">&copy; copyright @ <?= date('Y'); ?> by <span>Mr.V Kashi Reddy & Mr.Channa</span> | All rights reserved!</div>
+
+</footer>
+
+<!-- footer section ends -->
